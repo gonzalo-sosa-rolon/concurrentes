f709e5947f1f7c5b3e4905adb154383cca675d0d
maxi <maxi@maxi-desktop>
2012-10-13 16:11:01 -0300

    probando si andan los commits

---
+++ git/src/PruebaCommit.cpp
@@ -0,0 +1,18 @@
+/*
+ * PruebaCommit.cpp
+ *
+ *  Created on: 13/10/2012
+ *      Author: maxi
+ */
+
+#include "PruebaCommit.h"
+
+PruebaCommit::PruebaCommit() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PruebaCommit::~PruebaCommit() {
+	// TODO Auto-generated destructor stub
+}
+
